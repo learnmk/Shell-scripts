96d1130468f6da676184b8750f5fac931f38aa4b
learnmk <125124581+learnmk@users.noreply.github.com>
2023-11-05 16:40:50 +0530

    Create Jenkins.sh

---
+++ Jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Use this script to install Jenkins on RHEL server.
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install fontconfig java-17-openjdk
+sudo yum install jenkins
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+echo "Jenkins Initial admin password : $(cat /var/lib/jenkins/secrets/initialAdminPassword)"
